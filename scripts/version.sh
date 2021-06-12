ef903ac5ca2f0e911f0e47e007dc9f31d48b5df3
Jeeva Kandasamy <jkandasa@gmail.com>
2021-06-12 21:19:02 +0530

    fix version issue
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,7 @@ export BUILD_DATE=`date -u +'%Y-%m-%dT%H:%M:%S%:z'`
 export GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 export GIT_SHA=`git rev-parse HEAD`
 export GIT_SHA_SHORT=`git rev-parse --short HEAD`
-export VERSION_PKG="github.com/mycontroller-org/backend/v2/pkg/version"
+export VERSION_PKG="github.com/mycontroller-org/2mqtt/pkg/version"
 
 export LD_FLAGS="-X $VERSION_PKG.version=$GIT_BRANCH -X $VERSION_PKG.buildDate=$BUILD_DATE -X $VERSION_PKG.gitCommit=$GIT_SHA"
 
