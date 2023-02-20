6558da6590f13ef76de1ae673f74200b9dcc295f
Jeeva Kandasamy <jkandasa@gmail.com>
2023-02-20 07:23:12 +0530

    add command to generate sample config
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

---
+++ cmd/sub/generate_config.go
@@ -0,0 +1,62 @@
+package sub
+
+import (
+	"fmt"
+	"log"
+
+	cfgTY "github.com/mycontroller-org/2mqtt/pkg/types/config"
+	"github.com/mycontroller-org/server/v2/pkg/types/cmap"
+	"gopkg.in/yaml.v3"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(generateConfigCmd)
+}
+
+var generateConfigCmd = &cobra.Command{
+	Use:  "generate-config",
+	Long: "Generates sample config file",
+	Run: func(cmd *cobra.Command, args []string) {
+		sampleConfig := cfgTY.Config{
+			Logger: cfgTY.LoggerConfig{
+				Mode:     "development",
+				Encoding: "console",
+				Level:    "info",
+			},
+			Adapters: []cfgTY.AdapterConfig{
+				{
+					Name:           "my_first_adapter",
+					Enabled:        true,
+					ReconnectDelay: "30s",
+					Provider:       "raw",
+					Source: cmap.CustomMap{
+						"type":               "serial",
+						"port":               "/dev/ttyUSB0",
+						"baud_rate":          "115200",
+						"transmit_pre_delay": "10ms",
+					},
+					MQTT: cmap.CustomMap{
+						"broker":               "tcp://192.168.10.21:1883",
+						"insecure_skip_verify": "false",
+						"username":             "",
+						"password":             "",
+						"subscribe":            "receive_data/#",
+						"publish":              "publish_data",
+						"qos":                  "0",
+						"transmit_pre_delay":   "0s",
+						"reconnect_delay":      "5s",
+					},
+					FormatterScript: cfgTY.FormatterScript{},
+				},
+			},
+		}
+
+		data, err := yaml.Marshal(&sampleConfig)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(data))
+	},
+}
